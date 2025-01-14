9bcc88dd421de73aff485ed4ffbf7c6bb01067c7
Harald Eilertsen <haraldei@anduin.net>
2025-01-14 10:43:12 +0100

    Sync test/jdk/java/nio with upstream jdk24 branch.

--- test/jdk/java/nio/channels/Selector/LotsOfChannels.java
+++ test/jdk/java/nio/channels/Selector/LotsOfChannels.java
@@ -49,7 +49,6 @@ public class LotsOfChannels {
         Pipe[] pipes = new Pipe[PIPES_COUNT];
         Pipe pipe = Pipe.open();
         Pipe.SinkChannel sink = pipe.sink();
-        sink.configureBlocking(false);
         Pipe.SourceChannel source = pipe.source();
         Selector sel = Selector.open();
         source.configureBlocking(false);
