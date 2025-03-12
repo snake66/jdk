6190e07bc2e880b8a0fbdfc8560ca2e5275d989c
Harald Eilertsen <haraldei@anduin.net>
2025-03-12 11:48:10 +0100

    Enable IPv6 dual protocol socket support on FreeBSD
    It is possible to limit the jvm to default to IPv4 only sockets by
    setting `java.net.preferIPv4Stack` to true. This is the default on
    OpenBSD, but for otehr BSD's we follow the Java specification that
    sockets should accept both IPv4 and IPv6 when the latter is available.
    
    This work is sponsored by The FreeBSD Foundation

--- src/java.base/share/native/libjava/System.c
+++ src/java.base/share/native/libjava/System.c
@@ -194,6 +194,10 @@ Java_jdk_internal_util_SystemProps_00024Raw_platformProperties(JNIEnv *env, jcla
     }
 #endif
 
+#ifdef __OpenBSD__
+    PUTPROP(propArray, _java_net_preferIPV4Stack_NDX, sprops->java_net_preferIPv4Stack);
+#endif
+
     /* data model */
     if (sizeof(sprops) == 4) {
         sprops->data_model = "32";
