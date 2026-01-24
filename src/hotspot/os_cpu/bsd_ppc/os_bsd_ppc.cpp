cf5ac100631c79101c548610243e8ecd25d9a55d
Greg Lewis <glewis@eyesbeyond.com>
2026-01-24 14:03:10 -0800

    Fix build on FreeBSD/ppc64

--- src/hotspot/os_cpu/bsd_ppc/os_bsd_ppc.cpp
+++ src/hotspot/os_cpu/bsd_ppc/os_bsd_ppc.cpp
@@ -179,7 +179,7 @@ intptr_t* os::fetch_bcp_from_context(const void* ucVoid) {
   assert(ucVoid != nullptr, "invariant");
   const ucontext_t* uc = (const ucontext_t*)ucVoid;
   assert(os::Posix::ucontext_is_interpreter(uc), "invariant");
-  return reinterpret_cast<intptr_t*>(uc->uc_mcontext.regs->gpr[14]); // R14_bcp
+  return reinterpret_cast<intptr_t*>(uc->uc_mcontext.mc_gpr[14]); // R14_bcp
 }
 
 frame os::get_sender_for_C_frame(frame* fr) {
