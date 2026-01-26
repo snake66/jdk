snake66/jdk

commit d3083ac05453c9dd303038f90ddab50d52124e51

8371194: serviceability/sa/TestJhsdbJstackMixedWithXComp.java failing
Co-authored-by: Patricio Chilano Mateo <pchilanomate@openjdk.org>
Reviewed-by: cjplummer, pchilanomate

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/bsd/BsdDebuggerLocal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,9 @@
 package sun.jvm.hotspot.debugger.bsd;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import sun.jvm.hotspot.debugger.Address;
 import sun.jvm.hotspot.debugger.DebuggerBase;
@@ -75,9 +77,10 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
     // CDebugger support
     private BsdCDebugger cdbg;
 
-    // threadList and loadObjectList are filled by attach0 method
-    private List<ThreadProxy> threadList;
+    // loadObjectList is filled by attach0 method
     private List<LoadObject> loadObjectList;
+
+    private List<JavaThread> javaThreadList;
 
     // called by native method lookupByAddress0
     private ClosestSymbol createClosestSymbol(String name, long offset) {
@@ -241,10 +244,21 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
         }
     }
 
+    private void fillJavaThreadList() {
+      // TODO: thread list on macOS is now supported for corefile only.
+      if (!isCore && isDarwin) {
+          javaThreadList = Collections.emptyList();
+      } else {
+          Threads threads = VM.getVM().getThreads();
+          javaThreadList = IntStream.range(0, threads.getNumberOfThreads())
+                                    .mapToObj(threads::getJavaThreadAt)
+                                    .toList();
+      }
+    }
+
     /** From the Debugger interface via JVMDebugger */
     public synchronized void attach(int processID) throws DebuggerException {
         checkAttached();
-        threadList = new ArrayList<>();
         loadObjectList = new ArrayList<>();
         class AttachTask implements WorkerThreadTask {
            int pid;
@@ -264,7 +278,6 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
     /** From the Debugger interface via JVMDebugger */
     public synchronized void attach(String execName, String coreName) {
         checkAttached();
-        threadList = new ArrayList<>();
         loadObjectList = new ArrayList<>();
         attach0(execName, coreName);
         attached = true;
@@ -278,7 +291,7 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
             return false;
         }
 
-        threadList = null;
+        javaThreadList = null;
         loadObjectList = null;
 
         if (isCore) {
@@ -492,7 +505,12 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
     /** From the BsdCDebugger interface */
     public List<ThreadProxy> getThreadList() {
       requireAttach();
-      return threadList;
+      if (javaThreadList == null) {
+        fillJavaThreadList();
+      }
+      return javaThreadList.stream()
+                           .map(JavaThread::getThreadProxy)
+                           .toList();
     }
 
     /** From the BsdCDebugger interface */
@@ -561,21 +579,19 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
     /** this functions used for core file reading and called from native attach0,
         it returns an array of long integers as
         [thread_id, stack_start, stack_end, thread_id, stack_start, stack_end, ....] for
-        all java threads recorded in Threads. Also adds the ThreadProxy to threadList */
+        all java threads recorded in Threads. */
     public long[] getJavaThreadsInfo() {
         requireAttach();
-        Threads threads = VM.getVM().getThreads();
-        int len = threads.getNumberOfThreads();
-        long[] result = new long[len * 3];    // triple
+        if (javaThreadList == null) {
+            fillJavaThreadList();
+        }
+        long[] result = new long[javaThreadList.size() * 3];    // triple
         long beg, end;
         int i = 0;
-        for (int k = 0; k < threads.getNumberOfThreads(); k++) {
-            JavaThread t = threads.getJavaThreadAt(k);
+        for (var t : javaThreadList) {
             end = t.getStackBaseValue();
             beg = end - t.getStackSize();
-            BsdThread bsdt = (BsdThread)t.getThreadProxy();
-            long uid = bsdt.getUniqueThreadId();
-            if (threadList != null) threadList.add(bsdt);
+            long uid = ((BsdThread)t.getThreadProxy()).getUniqueThreadId();
             result[i] = uid;
             result[i + 1] = beg;
             result[i + 2] = end;

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/cdbg/CFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,8 @@
 package sun.jvm.hotspot.debugger.cdbg;
 
 import sun.jvm.hotspot.debugger.*;
+import sun.jvm.hotspot.runtime.*;
+import sun.jvm.hotspot.utilities.*;
 
 /** Models a "C" programming language frame on the stack -- really
     just an arbitrary frame with hooks to access C and C++ debug
@@ -37,7 +39,7 @@ public interface CFrame {
   public CFrame sender(ThreadProxy th);
 
   /** Find sender frame with given FP and PC */
-  public default CFrame sender(ThreadProxy th, Address fp, Address pc) {
+  public default CFrame sender(ThreadProxy th, Address sp, Address fp, Address pc) {
     return sender(th);
   }
 
@@ -70,4 +72,9 @@ public interface CFrame {
   /** Visit all local variables in this frame if debug information is
       available. Automatically descends into compound types and arrays. */
   public void iterateLocals(ObjectVisitor v);
+
+  /** Get Frame instance assosiated with this CFrame. */
+  public default Frame toFrame() {
+    throw new UnsupportedPlatformException();
+  }
 }

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/linux/LinuxCDebugger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2015, Red Hat Inc.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -81,9 +81,11 @@ class LinuxCDebugger implements CDebugger {
     String cpu = dbg.getCPU();
     if (cpu.equals("amd64")) {
        AMD64ThreadContext context = (AMD64ThreadContext) thread.getContext();
+       Address sp  = context.getRegisterAsAddress(AMD64ThreadContext.RSP);
+       if (sp == null) return null;
        Address pc  = context.getRegisterAsAddress(AMD64ThreadContext.RIP);
        if (pc == null) return null;
-       return LinuxAMD64CFrame.getTopFrame(dbg, pc, context);
+       return LinuxAMD64CFrame.getTopFrame(dbg, sp, pc, context);
     }  else if (cpu.equals("ppc64")) {
         PPC64ThreadContext context = (PPC64ThreadContext) thread.getContext();
         Address sp = context.getRegisterAsAddress(PPC64ThreadContext.SP);
@@ -93,18 +95,22 @@ class LinuxCDebugger implements CDebugger {
         return new LinuxPPC64CFrame(dbg, sp, pc, LinuxDebuggerLocal.getAddressSize());
     } else if (cpu.equals("aarch64")) {
        AARCH64ThreadContext context = (AARCH64ThreadContext) thread.getContext();
+       Address sp = context.getRegisterAsAddress(AARCH64ThreadContext.SP);
+       if (sp == null) return null;
        Address fp = context.getRegisterAsAddress(AARCH64ThreadContext.FP);
        if (fp == null) return null;
        Address pc  = context.getRegisterAsAddress(AARCH64ThreadContext.PC);
        if (pc == null) return null;
-       return new LinuxAARCH64CFrame(dbg, fp, pc);
+       return new LinuxAARCH64CFrame(dbg, sp, fp, pc);
     } else if (cpu.equals("riscv64")) {
        RISCV64ThreadContext context = (RISCV64ThreadContext) thread.getContext();
+       Address sp = context.getRegisterAsAddress(RISCV64ThreadContext.SP);
+       if (sp == null) return null;
        Address fp = context.getRegisterAsAddress(RISCV64ThreadContext.FP);
        if (fp == null) return null;
        Address pc  = context.getRegisterAsAddress(RISCV64ThreadContext.PC);
        if (pc == null) return null;
-       return new LinuxRISCV64CFrame(dbg, fp, pc);
+       return new LinuxRISCV64CFrame(dbg, sp, fp, pc);
     } else {
        // Runtime exception thrown by LinuxThreadContextFactory if unknown cpu
        ThreadContext context = thread.getContext();

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/linux/aarch64/LinuxAARCH64CFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2015, Red Hat Inc.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -30,10 +30,14 @@ import sun.jvm.hotspot.debugger.aarch64.*;
 import sun.jvm.hotspot.debugger.linux.*;
 import sun.jvm.hotspot.debugger.cdbg.*;
 import sun.jvm.hotspot.debugger.cdbg.basic.*;
+import sun.jvm.hotspot.code.*;
+import sun.jvm.hotspot.runtime.*;
+import sun.jvm.hotspot.runtime.aarch64.*;
 
 public final class LinuxAARCH64CFrame extends BasicCFrame {
-   public LinuxAARCH64CFrame(LinuxDebugger dbg, Address fp, Address pc) {
+   public LinuxAARCH64CFrame(LinuxDebugger dbg, Address sp, Address fp, Address pc) {
       super(dbg.getCDebugger());
+      this.sp = sp;
       this.fp = fp;
       this.pc = pc;
       this.dbg = dbg;
@@ -55,11 +59,11 @@ public final class LinuxAARCH64CFrame extends BasicCFrame {
 
    @Override
    public CFrame sender(ThreadProxy thread) {
-      return sender(thread, null, null);
+      return sender(thread, null, null, null);
    }
 
    @Override
-   public CFrame sender(ThreadProxy thread, Address nextFP, Address nextPC) {
+   public CFrame sender(ThreadProxy thread, Address nextSP, Address nextFP, Address nextPC) {
       // Check fp
       // Skip if both nextFP and nextPC are given - do not need to load from fp.
       if (nextFP == null && nextPC == null) {
@@ -86,7 +90,32 @@ public final class LinuxAARCH64CFrame extends BasicCFrame {
       if (nextPC == null) {
         return null;
       }
-      return new LinuxAARCH64CFrame(dbg, nextFP, nextPC);
+
+      if (nextSP == null) {
+        CodeCache cc = VM.getVM().getCodeCache();
+        CodeBlob currentBlob = cc.findBlobUnsafe(pc());
+
+        // This case is different from HotSpot. See JDK-8371194 for details.
+        if (currentBlob != null && (currentBlob.isContinuationStub() || currentBlob.isNativeMethod())) {
+          // Use FP since it should always be valid for these cases.
+          // TODO: These should be walked as Frames not CFrames.
+          nextSP = fp.addOffsetTo(2 * ADDRESS_SIZE);
+        } else {
+          CodeBlob codeBlob = cc.findBlobUnsafe(nextPC);
+          boolean useCodeBlob = codeBlob != null && codeBlob.getFrameSize() > 0;
+          nextSP = useCodeBlob ? nextFP.addOffsetTo((2 * ADDRESS_SIZE) - codeBlob.getFrameSize()) : nextFP;
+        }
+      }
+      if (nextSP == null) {
+        return null;
+      }
+
+      return new LinuxAARCH64CFrame(dbg, nextSP, nextFP, nextPC);
+   }
+
+   @Override
+   public Frame toFrame() {
+     return new AARCH64Frame(sp, fp, pc);
    }
 
    // package/class internals only

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/linux/amd64/LinuxAMD64CFrame.java
@@ -29,10 +29,12 @@ import sun.jvm.hotspot.debugger.amd64.*;
 import sun.jvm.hotspot.debugger.linux.*;
 import sun.jvm.hotspot.debugger.cdbg.*;
 import sun.jvm.hotspot.debugger.cdbg.basic.*;
+import sun.jvm.hotspot.runtime.*;
+import sun.jvm.hotspot.runtime.amd64.*;
 
 public final class LinuxAMD64CFrame extends BasicCFrame {
 
-   public static LinuxAMD64CFrame getTopFrame(LinuxDebugger dbg, Address rip, ThreadContext context) {
+   public static LinuxAMD64CFrame getTopFrame(LinuxDebugger dbg, Address rsp, Address rip, ThreadContext context) {
       Address libptr = dbg.findLibPtrByAddress(rip);
       Address cfa = context.getRegisterAsAddress(AMD64ThreadContext.RBP);
       DwarfParser dwarf = null;
@@ -45,7 +47,7 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
           // DWARF processing should succeed when the frame is native
           // but it might fail if Common Information Entry (CIE) has language
           // personality routine and/or Language Specific Data Area (LSDA).
-          return new LinuxAMD64CFrame(dbg, cfa, rip, dwarf, true);
+          return new LinuxAMD64CFrame(dbg, rsp, cfa, rip, dwarf, true);
         }
 
         cfa = context.getRegisterAsAddress(dwarf.getCFARegister())
@@ -53,19 +55,20 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
       }
 
       return (cfa == null) ? null
-                           : new LinuxAMD64CFrame(dbg, cfa, rip, dwarf);
+                           : new LinuxAMD64CFrame(dbg, rsp, cfa, rip, dwarf);
    }
 
-   private LinuxAMD64CFrame(LinuxDebugger dbg, Address cfa, Address rip, DwarfParser dwarf) {
-      this(dbg, cfa, rip, dwarf, false);
+   private LinuxAMD64CFrame(LinuxDebugger dbg, Address rsp, Address cfa, Address rip, DwarfParser dwarf) {
+      this(dbg, rsp, cfa, rip, dwarf, false);
    }
 
-   private LinuxAMD64CFrame(LinuxDebugger dbg, Address cfa, Address rip, DwarfParser dwarf, boolean finalFrame) {
-      this(dbg, cfa, rip, dwarf, finalFrame, false);
+   private LinuxAMD64CFrame(LinuxDebugger dbg, Address rsp, Address cfa, Address rip, DwarfParser dwarf, boolean finalFrame) {
+      this(dbg, rsp, cfa, rip, dwarf, finalFrame, false);
    }
 
-   private LinuxAMD64CFrame(LinuxDebugger dbg, Address cfa, Address rip, DwarfParser dwarf, boolean finalFrame, boolean use1ByteBeforeToLookup) {
+   private LinuxAMD64CFrame(LinuxDebugger dbg, Address rsp, Address cfa, Address rip, DwarfParser dwarf, boolean finalFrame, boolean use1ByteBeforeToLookup) {
       super(dbg.getCDebugger());
+      this.rsp = rsp;
       this.cfa = cfa;
       this.rip = rip;
       this.dbg = dbg;
@@ -107,7 +110,14 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
          (!isNative || (isNative && nextCFA.greaterThan(cfa)));
    }
 
-   private Address getNextCFA(DwarfParser nextDwarf, ThreadContext context, Address senderFP) {
+   private Address getNextRSP() {
+     // next RSP should be previous slot of return address.
+     var bp = dwarf == null ? cfa.addOffsetTo(ADDRESS_SIZE) // top of BP points callser BP
+                            : cfa.addOffsetTo(dwarf.getReturnAddressOffsetFromCFA());
+     return bp.addOffsetTo(ADDRESS_SIZE);
+   }
+
+   private Address getNextCFA(DwarfParser nextDwarf, ThreadContext context, Address senderFP, Address senderPC) {
      Address nextCFA;
      boolean isNative = false;
 
@@ -115,13 +125,17 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
        senderFP = cfa.getAddressAt(0);  // RBP by default
      }
 
-     if (nextDwarf == null) { // Next frame is Java
+     if (VM.getVM().getCodeCache().contains(senderPC)) { // Next frame is Java
        nextCFA = (dwarf == null) ? senderFP // Current frame is Java
                                  : cfa.getAddressAt(dwarf.getBasePointerOffsetFromCFA()); // Current frame is Native
      } else { // Next frame is Native
-       if (dwarf == null) { // Current frame is Java
+       if (VM.getVM().getCodeCache().contains(pc())) { // Current frame is Java
          nextCFA = senderFP.addOffsetTo(-nextDwarf.getBasePointerOffsetFromCFA());
        } else { // Current frame is Native
+         if (nextDwarf == null) { // maybe runtime entrypoint (_start())
+           throw new DebuggerException("nextDwarf is null even though native call");
+         }
+
          isNative = true;
          int nextCFAReg = nextDwarf.getCFARegister();
          if (nextCFAReg == AMD64ThreadContext.RBP) {
@@ -130,10 +144,7 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
            Address nextRBP = rbp.getAddressAt(0);
            nextCFA = nextRBP.addOffsetTo(-nextDwarf.getBasePointerOffsetFromCFA());
          } else if (nextCFAReg == AMD64ThreadContext.RSP) {
-           // next RSP should be previous slot of return address.
-           Address nextRSP = cfa.addOffsetTo(dwarf.getReturnAddressOffsetFromCFA())
-                                .addOffsetTo(ADDRESS_SIZE);
-           nextCFA = nextRSP.addOffsetTo(nextDwarf.getCFAOffset());
+           nextCFA = getNextRSP().addOffsetTo(nextDwarf.getCFAOffset());
          } else {
            throw new DebuggerException("Unsupported CFA register: " + nextCFAReg);
          }
@@ -153,16 +164,21 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
 
    @Override
    public CFrame sender(ThreadProxy th) {
-     return sender(th, null, null);
+     return sender(th, null, null, null);
    }
 
    @Override
-   public CFrame sender(ThreadProxy th, Address fp, Address pc) {
+   public CFrame sender(ThreadProxy th, Address sp, Address fp, Address pc) {
      if (finalFrame) {
        return null;
      }
 
      ThreadContext context = th.getContext();
+
+     Address nextRSP = sp != null ? sp : getNextRSP();
+     if (nextRSP == null) {
+       return null;
+     }
 
      Address nextPC = pc != null ? pc : getNextPC(dwarf != null);
      if (nextPC == null) {
@@ -183,13 +199,16 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
          // DWARF processing should succeed when the frame is native
          // but it might fail if Common Information Entry (CIE) has language
          // personality routine and/or Language Specific Data Area (LSDA).
-         return new LinuxAMD64CFrame(dbg, null, nextPC, nextDwarf, true);
+         return null;
        }
      }
 
-     Address nextCFA = getNextCFA(nextDwarf, context, fp);
-     return nextCFA == null ? null
-                            : new LinuxAMD64CFrame(dbg, nextCFA, nextPC, nextDwarf, false, fallback);
+     try {
+       Address nextCFA = getNextCFA(nextDwarf, context, fp, nextPC);
+       return new LinuxAMD64CFrame(dbg, nextRSP, nextCFA, nextPC, nextDwarf, false, fallback);
+     } catch (DebuggerException _) {
+       return null;
+     }
    }
 
    private DwarfParser createDwarfParser(Address pc) throws DebuggerException {
@@ -210,8 +229,14 @@ public final class LinuxAMD64CFrame extends BasicCFrame {
      return nextDwarf;
    }
 
+   @Override
+   public Frame toFrame() {
+     return new AMD64Frame(rsp, cfa, rip);
+   }
+
    // package/class internals only
    private static final int ADDRESS_SIZE = 8;
+   private Address rsp;
    private Address rip;
    private Address cfa;
    private LinuxDebugger dbg;

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/linux/ppc64/LinuxPPC64CFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,6 +29,8 @@ import sun.jvm.hotspot.debugger.ppc64.*;
 import sun.jvm.hotspot.debugger.linux.*;
 import sun.jvm.hotspot.debugger.cdbg.*;
 import sun.jvm.hotspot.debugger.cdbg.basic.*;
+import sun.jvm.hotspot.runtime.*;
+import sun.jvm.hotspot.runtime.ppc64.*;
 
 public final class LinuxPPC64CFrame extends BasicCFrame {
   // package/class internals only
@@ -69,6 +71,12 @@ public final class LinuxPPC64CFrame extends BasicCFrame {
       return null;
     }
     return new LinuxPPC64CFrame(dbg, nextSP, nextPC, address_size);
+  }
+
+  @Override
+  public Frame toFrame() {
+    // 2nd arg (raw_fp) would be derived from sp in c'tor of PPC64Frame.
+    return new PPC64Frame(sp, null, pc);
   }
 
   public static int PPC64_STACK_BIAS = 0;

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/linux/riscv64/LinuxRISCV64CFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2015, Red Hat Inc.
  * Copyright (c) 2021, Huawei Technologies Co., Ltd. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -31,13 +31,17 @@ import sun.jvm.hotspot.debugger.riscv64.*;
 import sun.jvm.hotspot.debugger.linux.*;
 import sun.jvm.hotspot.debugger.cdbg.*;
 import sun.jvm.hotspot.debugger.cdbg.basic.*;
+import sun.jvm.hotspot.runtime.*;
+import sun.jvm.hotspot.runtime.riscv64.*;
 
 public final class LinuxRISCV64CFrame extends BasicCFrame {
    private static final int C_FRAME_LINK_OFFSET        = -2;
    private static final int C_FRAME_RETURN_ADDR_OFFSET = -1;
+   private static final int C_FRAME_SENDER_SP_OFFSET   =  0;
 
-   public LinuxRISCV64CFrame(LinuxDebugger dbg, Address fp, Address pc) {
+   public LinuxRISCV64CFrame(LinuxDebugger dbg, Address sp, Address fp, Address pc) {
       super(dbg.getCDebugger());
+      this.sp = sp;
       this.fp = fp;
       this.pc = pc;
       this.dbg = dbg;
@@ -59,11 +63,11 @@ public final class LinuxRISCV64CFrame extends BasicCFrame {
 
    @Override
    public CFrame sender(ThreadProxy thread) {
-      return sender(thread, null, null);
+      return sender(thread, null, null, null);
    }
 
    @Override
-   public CFrame sender(ThreadProxy thread, Address nextFP, Address nextPC) {
+   public CFrame sender(ThreadProxy thread, Address nextSP, Address nextFP, Address nextPC) {
       // Check fp
       // Skip if both nextFP and nextPC are given - do not need to load from fp.
       if (nextFP == null && nextPC == null) {
@@ -75,6 +79,13 @@ public final class LinuxRISCV64CFrame extends BasicCFrame {
         if (dbg.getAddressValue(fp) % (2 * ADDRESS_SIZE) != 0) {
           return null;
         }
+      }
+
+      if (nextSP == null) {
+        nextSP = fp.getAddressAt(C_FRAME_SENDER_SP_OFFSET * ADDRESS_SIZE);
+      }
+      if (nextSP == null) {
+        return null;
       }
 
       if (nextFP == null) {
@@ -91,7 +102,12 @@ public final class LinuxRISCV64CFrame extends BasicCFrame {
         return null;
       }
 
-      return new LinuxRISCV64CFrame(dbg, nextFP, nextPC);
+      return new LinuxRISCV64CFrame(dbg, nextSP, nextFP, nextPC);
+   }
+
+   @Override
+   public Frame toFrame() {
+      return new RISCV64Frame(sp, fp, pc);
    }
 
    // package/class internals only

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/runtime/aarch64/AARCH64Frame.java
@@ -272,9 +272,7 @@ public class AARCH64Frame extends Frame {
     if (cb != null) {
       if (cb.isUpcallStub()) {
         return senderForUpcallStub(map, (UpcallStub)cb);
-      } else if (cb.isContinuationStub()) {
-        return senderForContinuationStub(map, cb);
-      } else {
+      } else if (cb.getFrameSize() > 0) {
         return senderForCompiledFrame(map, cb);
       }
     }
@@ -389,7 +387,11 @@ public class AARCH64Frame extends Frame {
     if (Assert.ASSERTS_ENABLED) {
         Assert.that(cb.getFrameSize() > 0, "must have non-zero frame size");
     }
-    Address senderSP = getUnextendedSP().addOffsetTo(cb.getFrameSize());
+
+    // TODO: senderSP should consider not only PreserveFramePointer but also _sp_is_trusted.
+    Address senderSP = !VM.getVM().getCommandLineBooleanFlag("PreserveFramePointer")
+                           ? getUnextendedSP().addOffsetTo(cb.getFrameSize())
+                           : getSenderSP();
 
     // The return_address is always the word on the stack
     Address senderPC = stripPAC(senderSP.getAddressAt(-1 * VM.getVM().getAddressSize()));

commit e988f694dfb4df73163492dd13eeb5c5eb6c4853

Fix sa agent on FreeBSD part 1 (#34)
This restores the state of the Serviceability Agent on FreeBSD to more or less the same state as it was before being broken by commit d3083ac.

Thanks to glewis and bsdkurt for valuable feedback!

This work was sponsored by: The FreeBSD Foundation

src/jdk.hotspot.agent/bsd/native/libsaproc/BsdDebuggerLocal.cpp
@@ -35,20 +35,8 @@
 #define amd64 1
 #endif
 
-#if defined(i386) && !defined(i586)
-#define i586 1
-#endif
-
-#ifdef i586
-#include "sun_jvm_hotspot_debugger_x86_X86ThreadContext.h"
-#endif
-
 #ifdef amd64
 #include "sun_jvm_hotspot_debugger_amd64_AMD64ThreadContext.h"
-#endif
-
-#if defined(sparc) || defined(sparcv9)
-#include "sun_jvm_hotspot_debugger_sparc_SPARCThreadContext.h"
 #endif
 
 #if defined(ppc64) || defined(ppc64le)
@@ -115,7 +103,6 @@ struct ps_prochandle* get_proc_handle(JNIEnv* env, jobject this_obj) {
 extern "C"
 JNIEXPORT void JNICALL Java_sun_jvm_hotspot_debugger_bsd_BsdDebuggerLocal_init0
   (JNIEnv *env, jclass cls) {
-  jclass listClass;
 
   if (init_libproc(getenv("LIBSAPROC_DEBUG") != NULL) != true) {
      THROW_NEW_DEBUGGER_EXCEPTION("can't initialize libproc");
@@ -139,8 +126,9 @@ JNIEXPORT void JNICALL Java_sun_jvm_hotspot_debugger_bsd_BsdDebuggerLocal_init0
   getThreadForThreadId_ID = env->GetMethodID(cls, "getThreadForThreadId",
                                                      "(J)Lsun/jvm/hotspot/debugger/ThreadProxy;");
   CHECK_EXCEPTION;
+
   // java.util.List method we call
-  listClass = env->FindClass("java/util/List");
+  jclass listClass = env->FindClass("java/util/List");
   CHECK_EXCEPTION;
   listAdd_ID = env->GetMethodID(listClass, "add", "(Ljava/lang/Object;)Z");
   CHECK_EXCEPTION;

src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/bsd/BsdDebuggerLocal.java
@@ -80,7 +80,11 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
     // loadObjectList is filled by attach0 method
     private List<LoadObject> loadObjectList;
 
+    // MacOS code uses this thread list
     private List<JavaThread> javaThreadList;
+
+    // BSD code uses this thread list
+    private List<ThreadProxy> threadList;
 
     // called by native method lookupByAddress0
     private ClosestSymbol createClosestSymbol(String name, long offset) {
@@ -272,6 +276,7 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
     public synchronized void attach(int processID) throws DebuggerException {
         checkAttached();
         loadObjectList = new ArrayList<>();
+        threadList = new ArrayList<>();
         class AttachTask implements WorkerThreadTask {
            int pid;
            public void doit(BsdDebuggerLocal debugger) {
@@ -291,6 +296,7 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
     public synchronized void attach(String execName, String coreName) {
         checkAttached();
         loadObjectList = new ArrayList<>();
+        threadList = new ArrayList<>();
         attach0(execName, coreName);
         attached = true;
         isCore = true;
@@ -303,6 +309,7 @@ public class BsdDebuggerLocal extends DebuggerBase implements BsdDebugger {
             return false;
         }
 
+        threadList = null;
         javaThreadList = null;
         loadObjectList = null;
 